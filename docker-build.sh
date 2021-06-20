67971168a4b008298c150ba1ecdada4264114017
Camilo A Cifuentes <camilocifuentes95@gmail.com>
2021-06-20 11:59:40 -0500

    Added Travis Docker Build CI

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+DOCKER_BUILDKIT=1 docker build -t ccifuentes95/cerberus-api:latest -t ccifuentes95/cerberus-api:$SHA .
+docker push ccifuentes95/cerberus-api:latest
+docker push ccifuentes95/cerberus-api:$SHA
